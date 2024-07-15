c3a24f303a9637bcf8588dbfe266f8b3e9250f90
Tomohiro Komabayashi <tomohirokomabayashi@gmail.com>
2024-07-15 13:29:25 +0200

    Modified convert.py to generate episodes.yaml

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 import os
 import eyed3
 import yaml
+import time
 
 # read mp3 files from the audio directory and list them
 def read_mp3_files():
@@ -18,14 +19,26 @@ def get_audio_files():
             comments = ''
             if audio_file.tag.comments:
                 for comment in audio_file.tag.comments:
-                    comments += comment.text + '\n'
+                    comments += comment.text
+            duration = time.strftime('%H:%M:%S', time.gmtime(audio_file.info.time_secs))
+            size = os.path.getsize(os.path.join('audio', file))
+            size_str = '{:,}'.format(size)
             audio_files.append({
                 'title': audio_file.tag.title,
-                'comments': comments
+                'comments': comments,
+                'filename': '/audio/' + file,
+                'duration': duration,
+                'size': size_str
             })
     return audio_files
 
-print(yaml.dump(get_audio_files()))
+def convert_to_yaml():
+    data = get_audio_files()
+    yaml_data = yaml.dump(data, sort_keys=False)
+    with open('episodes.yaml', 'w') as f:
+        f.write(yaml_data)
+
+convert_to_yaml()
 
 '''
 def read_audio_list_from_files():
